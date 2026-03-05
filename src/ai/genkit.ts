3929b50049c1fd491e6ef0b5e35def4567f7c2aa
Tristen <TristenDBayley@gmail.com>
2026-03-05 21:41:00 +0000

    Set up a Firebase backend

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import {firebase} from '@genkit-ai/firebase';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI(), firebase()],
   model: 'googleai/gemini-2.5-flash',
 });
---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5500996433-ef29e",
+  "appId": "1:446406774331:web:0523342bff25ece1a3961e",
+  "apiKey": "AIzaSyB98KKjMuvRQxJRxqmKEYfVB9n6z40QnyY",
+  "authDomain": "studio-5500996433-ef29e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "446406774331"
+};
